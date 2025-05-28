8643d66c443fa8477f7660ac7752224304900e5e
Aymerick Michelet <micheletaymerick@gmail.com>
2025-05-28 13:29:11 +0200

    feat: build .app

--- build/compilateur.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo -Cr�ation ressources.o
-windres others\ressources.rc -o others\ressources.o
-echo Ressources.o OKAY
-
-echo -Cr�ation Mario.exe
-gcc src\*.c src\menu\*.c src\game\*.c src\game\mechant\*.c src\game\joueur\*.c others\ressources.o -o Mario.exe -I include -L lib -lmingw32 -lSDLmain -lSDL -lSDL_mixer -mwindows
-echo Mario.exe OKAY
-pause
